c7ab098d7c2e7388396f2bb495bcd717ced4d107
Bismarck <brunowasborn2rock@gmail.com>
2026-01-19 15:41:42 -0300

    django shell

---
+++ 2 - Zero to Hero Django/core/home/utils.py
@@ -0,0 +1,9 @@
+from home.models import *
+import time
+
+def run_this_function():
+    print("Running this function from utils.py")
+    time.sleep(2)
+    print("Function execution completed!")
+
+# Essa função deve ser executada dentro do shell do Django, não diretamente no navegador.

247bb7644f0b146defe90f1b98d4971518ea55c7
Bismarck <brunowasborn2rock@gmail.com>
2026-02-01 17:12:59 -0300

    custom user model

--- 2 - Zero to Hero Django/core/vege/views.py
+++ 2 - Zero to Hero Django/core/vege/views.py
@@ -8,6 +8,9 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q, Sum
 from .seed import generate_report_card
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your views here.
 @login_required(login_url='/login/')

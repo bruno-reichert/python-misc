86bbb77f401fc54c3faa45b27648d712cc14ade0
Bismarck <brunowasborn2rock@gmail.com>
2026-02-12 16:04:07 -0300

    the end of django 2

--- 3 - Django Full Course For Beginners/myproject/myproject/settings.py
+++ 3 - Django Full Course For Beginners/myproject/myproject/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,8 +24,8 @@ SECRET_KEY = 'django-insecure-g5n)#*qxs#%&3@04x-l8uq-)_hmhl@d(a1_9c^_yt9mzke3nrw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1'] - Used if DEBUG = False
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -118,7 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = BASE_DIR / 'assets'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [BASE_DIR / 'static']

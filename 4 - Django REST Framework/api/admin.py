5d03c3a32fb6dce9cecf8e2a6b44886aa673c350
Bismarck <brunowasborn2rock@gmail.com>
2026-02-17 16:10:03 -0300

    generics

--- 4 - Django REST Framework/api/admin.py
+++ 4 - Django REST Framework/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from api.models import Order, OrderItem
+
+class OrderAdminInline(admin.TabularInline): # Inlines são usados para exibir e editar objetos relacionados diretamente na página de administração do modelo pai. O TabularInline exibe os objetos relacionados em uma tabela, enquanto o StackedInline os exibe em um formato empilhado.
+    model = OrderItem
 
 # Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderAdminInline]
+
+admin.site.register(Order, OrderAdmin)

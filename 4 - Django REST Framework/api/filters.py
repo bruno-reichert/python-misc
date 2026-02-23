c350b86901268bd36d8778dc3b0fb39e7c67df89
Bismarck <brunowasborn2rock@gmail.com>
2026-02-23 15:46:56 -0300

    more filtering

--- 4 - Django REST Framework/api/filters.py
+++ 4 - Django REST Framework/api/filters.py
@@ -1,5 +1,6 @@
 import django_filters
 from api.models import Product
+from rest_framework import filters
 
 class ProductFilter(django_filters.FilterSet):
     class Meta:
@@ -8,3 +9,7 @@ class ProductFilter(django_filters.FilterSet):
             'name': ['icontains'], # Permite filtrar por nome usando query params, ex: /api/products/?name=example
             'price': ['exact', 'lt', 'gt', 'range'], # Permite filtrar por preço usando query params, ex: /api/products/?price=10 ou /api/products/?price__lt=10 ou /api/products/?price__gt=10 ou /api/products/?price__range=10,20 
         }
+
+class InStockFilterBackEnd(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(stock__gt=0)

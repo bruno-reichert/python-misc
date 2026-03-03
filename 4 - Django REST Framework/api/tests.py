eaf3853c7b2f29cc54c55eddf1bfddb3cc8d4eed
Bismarck <brunowasborn2rock@gmail.com>
2026-03-03 16:12:38 -0300

    testing 2

--- 4 - Django REST Framework/api/tests.py
+++ 4 - Django REST Framework/api/tests.py
@@ -1,27 +1,37 @@
-from django.test import TestCase
-from api.models import User, Order
+from api.models import User, Product
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Create your tests here.
-class UserOrderTestCase(TestCase):
+class ProductAPITestCase(APITestCase):
     def setUp(self):
-        user1 = User.objects.create_user(username='user1', password='test')
-        user2 = User.objects.create_user(username='user2', password='test')
-        Order.objects.create(user=user1)
-        Order.objects.create(user=user1)
-        Order.objects.create(user=user2)
-        Order.objects.create(user=user2)
+        self.admin_user = User.objects.create_superuser(username='testadmin', password='adminpass') # type: ignore
+        self.normal_user = User.objects.create_user(username='user', password='userpass') # type: ignore
+        self.product = Product.objects.create(name='Test Product', description='A product for testing', price=9.99, stock=10)
+        self.url = reverse('product-detail', kwargs={'product_id': self.product.pk}) 
 
-    def test_user_order_endpoint_retrieves_only_authenticated_user_orders(self):
-        user = User.objects.get(username='user2')
-        self.client.force_login(user)
-        response = self.client.get(reverse('user-orders'))
+    def test_get_product(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], self.product.name) # type: ignore
 
-        assert response.status_code == status.HTTP_200_OK
-        orders = response.json()
-        self.assertTrue(all(order['user'] == user.id for order in orders)) # type: ignore
-
-    def test_user_order_list_unauthenticated(self):
-        response = self.client.get(reverse('user-orders'))
+    def test_unauthorized_update_product(self):
+        data = {'name': 'Updated Product'}
+        response = self.client.put(self.url, data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_unauthorized_delete_product(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_only_admin_can_delete_product(self):
+        self.client.login(username='user', password='userpass')
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN) 
+        self.assertTrue(Product.objects.filter(pk=self.product.pk).exists())
+
+        self.client.login(username='testadmin', password='adminpass')
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT) 
+        self.assertFalse(Product.objects.filter(pk=self.product.pk).exists())

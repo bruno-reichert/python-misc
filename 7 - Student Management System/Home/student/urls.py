2c3f69d6f84dccd52fcfd180a3a9428fc16ed384
Bismarck <brunowasborn2rock@gmail.com>
2026-03-26 16:12:03 -0300

    models

--- 7 - Student Management System/Home/student/urls.py
+++ 7 - Student Management System/Home/student/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from school import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('school.urls')),
-    path('', include('student.urls'))
 ]

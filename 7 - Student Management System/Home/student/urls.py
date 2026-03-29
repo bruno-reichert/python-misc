256abbc4a7375a648d7c7a99b3220bee5dbb4f7a
Bismarck <brunowasborn2rock@gmail.com>
2026-03-29 15:44:27 -0300

    edit and delete students

--- 7 - Student Management System/Home/student/urls.py
+++ 7 - Student Management System/Home/student/urls.py
@@ -21,5 +21,7 @@ from student import views
 urlpatterns = [
     path('list/', views.student_list, name='student_list'),
     path('add/', views.add_student, name='add_student'),
-    path('students/<str:slug>/', views.view_student, name="view_student")
+    path('students/<str:slug>/', views.view_student, name="view_student"),
+    path('edit/<str:slug>/', views.edit_student, name="edit_student"), # type: ignore
+    path('delete/<str:slug>', views.delete_student, name="delete_student")
 ]

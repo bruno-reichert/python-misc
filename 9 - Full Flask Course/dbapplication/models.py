637e456cd8c8a43234db73bf53f8fb00d1c89984
Bismarck <brunowasborn2rock@gmail.com>
2026-04-13 16:32:53 -0300

    user authentication

--- 9 - Full Flask Course/dbapplication/models.py
+++ 9 - Full Flask Course/dbapplication/models.py
@@ -1,11 +1,16 @@
 from app import db
+from flask_login import UserMixin
 
-class Person(db.Model):
-    __tablename__ = 'people'
-    pid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False)
-    age = db.Column(db.Integer)
-    job = db.Column(db.Text)
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    uid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.String)
+    description = db.Column(db.String)
 
     def __repr__(self):
-        return f'Person(pid={self.pid}, name={self.name}, age={self.age}, job={self.job})'
+        return f"User: {self.username}, Role: {self.role}"
+
+    def get_id(self):
+        return str(self.uid)
--- 9 - Full Flask Course/dbapplication/routes.py
+++ 9 - Full Flask Course/dbapplication/routes.py
@@ -1,29 +1,48 @@
 from flask import render_template, request, redirect, url_for, jsonify
-from models import Person
+from models import User
+from flask_login import login_user, logout_user, current_user, login_required
 
-def register_routes(app, db):
-    @app.route('/', methods=['GET', 'POST'])
+
+def register_routes(app, db, bcrypt):
+    @app.route('/')
     def index():
-        if request.method == "GET": 
-            people = Person.query.all()
-            return render_template('index.html', people=people)
+        return render_template('index.html')
+    
+    @app.route('/signup', methods=['GET', 'POST'])
+    def signup():
+        if request.method == "GET":
+            return render_template('signup.html')
         else:
-            name = request.form['name']
-            age = int(request.form['age'])
-            job = request.form['job']
-            person = Person(name=name, age=age, job=job) # type: ignore
-            db.session.add(person)
+            username = request.form.get('username')
+            password = request.form.get('password')
+            hashed_password = bcrypt.generate_password_hash(password)
+            user = User(username=username, password=hashed_password) # type: ignore
+            db.session.add(user)
             db.session.commit()
-
             return redirect(url_for('index'))
         
-    @app.route('/delete/<pid>', methods=['DELETE'])
-    def delete(pid):
-        Person.query.filter_by(pid=pid).delete()
-        db.session.commit()
-        return jsonify({'success': True})
+    @app.route('/login/', methods=['GET', 'POST'])
+    def login():
+        if request.method == "GET":
+            return render_template('login.html')
+        else:
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            user = User.query.filter_by(username=username).first()
+            if bcrypt.check_password_hash(user.password, password): # type: ignore
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                return 'Failed'
+            
     
-    @app.route('/details/<pid>')
-    def details(pid):
-        person = Person.query.filter(Person.pid == pid).first()
-        return render_template('details.html', person=person)
+    @app.route('/logout', methods=['GET', 'POST'])
+    def logout():
+        logout_user()
+        return redirect(url_for('index'))
+    
+    @app.route('/secret')
+    @login_required
+    def secret():
+        return 'This is a secret page!'
